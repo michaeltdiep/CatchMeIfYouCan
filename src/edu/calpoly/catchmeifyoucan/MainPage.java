4a0ae5004e1996cd2794eebd0c12770062a75c21
cclark18 <cclark18@calpoly.edu>
2012-11-13 09:37:12 -0800

    starting to add button funtionality

--- src/edu/calpoly/catchmeifyoucan/MainPage.java
+++ src/edu/calpoly/catchmeifyoucan/MainPage.java
@@ -3,18 +3,31 @@ package edu.calpoly.catchmeifyoucan;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainPage extends Activity {
+public class MainPage extends Activity implements OnClickListener{
+	
+	Button buttonMainSnitch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        buttonMainSnitch = (Button)findViewById(R.id.button_main_snitch);
+        
+        buttonMainSnitch.setOnClickListener(this);
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main_page, menu);
         return true;
     }
+
+	public void onClick(View buttonChosen) {
+		// TODO Auto-generated method stub
+		
+	}
 }
