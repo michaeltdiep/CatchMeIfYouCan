ab4d6551b3ca17bd7e22799dee1474778a1c2217
cclark18 <cclark18@calpoly.edu>
2012-11-13 08:54:15 -0800

    Added snitch main activity

--- gen/edu/calpoly/catchmeifyoucan/R.java
+++ gen/edu/calpoly/catchmeifyoucan/R.java
@@ -20,10 +20,12 @@ public final class R {
     public static final class layout {
         public static final int activity_main_page=0x7f030000;
         public static final int activity_seeker_main_page=0x7f030001;
+        public static final int activity_snitch_main_page=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main_page=0x7f060000;
         public static final int activity_seeker_main_page=0x7f060001;
+        public static final int activity_snitch_main_page=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -31,6 +33,7 @@ public final class R {
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main_page=0x7f040003;
         public static final int title_activity_seeker_main_page=0x7f040004;
+        public static final int title_activity_snitch_main_page=0x7f040005;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
