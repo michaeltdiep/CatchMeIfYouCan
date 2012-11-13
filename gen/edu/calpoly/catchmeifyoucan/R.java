a1254b319c5aaccfba4f72705cae284e03a07fb1
cclark18 <cclark18@calpoly.edu>
2012-11-13 09:30:21 -0800

    added buttons on main

--- gen/edu/calpoly/catchmeifyoucan/R.java
+++ gen/edu/calpoly/catchmeifyoucan/R.java
@@ -15,7 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button_main_confused=0x7f070002;
+        public static final int button_main_seeker=0x7f070001;
+        public static final int button_main_snitch=0x7f070000;
+        public static final int menu_settings=0x7f070003;
     }
     public static final class layout {
         public static final int activity_confused=0x7f030000;
