53dc636a854c847b2edc7cf3a20a4b19b857842a
cclark18 <cclark18@calpoly.edu>
2012-11-14 22:16:02 -0800

    adding maps functionality

--- src/edu/calpoly/catchmeifyoucan/SeekerMap.java
+++ src/edu/calpoly/catchmeifyoucan/SeekerMap.java
@@ -1,13 +1,30 @@
 package edu.calpoly.catchmeifyoucan;
 
-import com.google.android.maps.MapActivity;
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.calpoly.catchmeifyoucan.MapsItemizedOverlay;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class SeekerMap extends MapActivity implements OnClickListener {
+	
+	ArrayList<GeoPoint> geoPoints = new ArrayList<GeoPoint>(); // used to dynamically store geopoints
+	
+	MapView mapView;                              // declaring these variables here (but not initializing them!)
+	MyLocationOverlay myLocationOverlay;          // allows them to be referenced in multiple methods
+	List<Overlay> mapOverlays;
+	MapsItemizedOverlay itemizedoverlay;
+	Drawable drawable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
