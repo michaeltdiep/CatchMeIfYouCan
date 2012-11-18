98da1446731c9ab8175810f185c6937bbf9ee613
ryanmoelter <ryanmoelter@live.com>
2012-11-18 13:43:58 -0800

    Modified main screen layout; changed icon and font and general layout.

--- gen/edu/calpoly/catchmeifyoucan/R.java
+++ gen/edu/calpoly/catchmeifyoucan/R.java
@@ -13,23 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int plus_icon_hdpi_bg=0x7f020002;
+        public static final int plus_icon_base=0x7f020002;
     }
     public static final class id {
-        public static final int button_main_confused=0x7f070002;
-        public static final int button_main_seeker=0x7f070000;
-        public static final int button_main_snitch=0x7f070001;
-        public static final int mapview=0x7f070007;
-        public static final int menu_settings=0x7f07000d;
-        public static final int progressBar1=0x7f07000b;
-        public static final int seeker_progress_bar=0x7f070009;
-        public static final int settings_button=0x7f07000c;
-        public static final int snitch_contact_picker=0x7f070004;
-        public static final int snitch_num=0x7f070005;
-        public static final int snitch_text_box=0x7f070003;
-        public static final int start_button=0x7f070006;
-        public static final int textView1=0x7f07000a;
-        public static final int waiting_for_snitch=0x7f070008;
+        public static final int button_main_confused=0x7f070005;
+        public static final int button_main_seeker=0x7f070001;
+        public static final int button_main_snitch=0x7f070003;
+        public static final int image_confused=0x7f070006;
+        public static final int image_seeker=0x7f070002;
+        public static final int image_snitch=0x7f070004;
+        public static final int mapview=0x7f07000b;
+        public static final int menu_settings=0x7f070011;
+        public static final int progressBar1=0x7f07000f;
+        public static final int seeker_progress_bar=0x7f07000d;
+        public static final int settings_button=0x7f070010;
+        public static final int snitch_contact_picker=0x7f070008;
+        public static final int snitch_num=0x7f070009;
+        public static final int snitch_text_box=0x7f070007;
+        public static final int start_button=0x7f07000a;
+        public static final int textView1=0x7f07000e;
+        public static final int text_main_i_am=0x7f070000;
+        public static final int waiting_for_snitch=0x7f07000c;
     }
     public static final class layout {
         public static final int activity_confused=0x7f030000;
