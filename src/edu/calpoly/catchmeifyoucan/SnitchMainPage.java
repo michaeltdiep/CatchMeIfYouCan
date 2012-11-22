47ae4f8f18248eecfc0d67955af8af11e3b6acab
ryanmoelter <ryanmoelter@live.com>
2012-11-22 01:23:21 -0800

    Mostly laid out the snitch main page (it might have an error, I'm just done for the night). Also, updated the seeker main page layout.

--- src/edu/calpoly/catchmeifyoucan/SnitchMainPage.java
+++ src/edu/calpoly/catchmeifyoucan/SnitchMainPage.java
@@ -6,10 +6,10 @@ import android.view.Menu;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+//import android.app.AlertDialog;
+//import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
+//import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,12 +17,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.graphics.Typeface;
 
 public class SnitchMainPage extends Activity implements OnClickListener{
 	
-	Button start;
-	Button settings;
+	RelativeLayout start;
+	RelativeLayout settings;
 	
+	// Typeface
+	TextView title;
+	Typeface thin;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,10 +35,16 @@ public class SnitchMainPage extends Activity implements OnClickListener{
         
         //Set up start and settings button
         //TODO start button doesn't actually lead to SnitchMap.class
-        start = (Button)findViewById(R.id.start_button);
+        start = (RelativeLayout)findViewById(R.id.start_button);
         start.setOnClickListener(this);
-        settings = (Button)findViewById(R.id.settings_button);
+        settings = (RelativeLayout)findViewById(R.id.snitch_settings_button);
         settings.setOnClickListener(this);
+        
+        // Typeface
+        thin = Typeface.createFromAsset(getAssets(), "roboto_thin.ttf");
+        title = (TextView)findViewById(R.id.text_snitch_title);
+        title.setTypeface(thin);
+        
     }
 
     @Override
