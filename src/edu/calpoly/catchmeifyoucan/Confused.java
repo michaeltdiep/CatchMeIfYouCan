54ee897dbe7255b927cee6fc457a099cda6c0c89
Ryan Moelter <ryanmoelter@live.com>
2012-11-27 21:25:06 -0800

    Done with the confused layout and text! :D

--- src/edu/calpoly/catchmeifyoucan/Confused.java
+++ src/edu/calpoly/catchmeifyoucan/Confused.java
@@ -11,9 +11,7 @@ import android.widget.TextView;
 public class Confused extends Activity {
 
 	Typeface thin, light;
-	TextView title, objectiveTitle, objectiveText, startTitle, startText,
-		snitchTitle, snitchText, seekerTitle, seekerText, ourGameTitle,
-		ourGameBody;
+	TextView title, objectiveTitle, startTitle, gameTitle, endTitle;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +28,10 @@ public class Confused extends Activity {
         objectiveTitle.setTypeface(light);
         startTitle = (TextView)findViewById(R.id.text_start_title);
         startTitle.setTypeface(light);
+        gameTitle = (TextView)findViewById(R.id.text_game_title);
+        gameTitle.setTypeface(light);
+        endTitle = (TextView)findViewById(R.id.text_end_title);
+        endTitle.setTypeface(light);
         
         
     }
